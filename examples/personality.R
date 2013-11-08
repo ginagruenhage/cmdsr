52f5e729f19935bcd574555fec1743c80c7168fb
ginagruenhage <gina.gruenhage@bccn-berlin.de>
2013-11-08 14:00:49 +0100

    Removed personality.R

--- examples/personality.R
+++
@@ -1,8 +0,0 @@
-dmat <- function(lw,up)
-    {
-        sub <- subset(bfi,age>lw & age < up)
-        X <- sub[,1:25]
-        complete <- do.call('c',alply(X,1,function(v) !any(is.na(v))))
-        XC <- as.matrix(X[complete,])
-        as.matrix(dist(t(XC)))/sqrt(nrow(XC))
-    }
